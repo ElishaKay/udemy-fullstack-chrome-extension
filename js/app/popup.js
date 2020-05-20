043b217bf9888dbb27be840f73ca7a818cda6e72
Elisha Kramer <kramer1346@gmail.com>
2020-05-20 08:53:11 +0300

    first commit

---
+++ js/app/popup.js
@@ -0,0 +1,15 @@
+console.log('popup.js loaded');
+
+let myAmazonHistory = angular.module("myamazonhistory", ['ui.router']);
+
+myAmazonHistory.config(function($stateProvider, $urlRouterProvider){
+
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: '../views/home.html'
+		})
+
+	$urlRouterProvider.otherwise('home')
+
+})
