01e247847a2ec616d4a79f096bb9b600a748d305
Elisha Kramer <kramer1346@gmail.com>
2020-06-02 11:39:02 +0300

    sending token in http requests to 'user/me' and getting back the authenticated user - login and signup tests passed

--- Chrome_Extension/js/app/background.js
+++ Chrome_Extension/js/app/background.js
@@ -2,6 +2,10 @@ console.log('background script ran');
 let dev = true;
 let domain = dev ? "http://localhost:8000/" : 'https://myamazonhistory.com/';
 
+ajaxCall("GET", "user/me", {}, getStorageItem('user') ? getStorageItem('user').token : '', function(response){
+    console.log('response from server is: ',response);
+})
+
 chrome.runtime.onMessage.addListener(
     function(message, sender, sendResponse) {
         switch(message.type) {
@@ -9,8 +13,9 @@ chrome.runtime.onMessage.addListener(
                 console.log('login logic ran with formData = to', message.data);
                 let userLoginCreds = message.data;
                 userLoginCreds.username = message.data.email.split('@')[0];
-                ajaxCall("POST", "user/login", userLoginCreds, function(response){
+                ajaxCall("POST", "user/login", userLoginCreds, '', function(response){
                    console.log('response from server is: ',response);
+                   setStorageItem('user',response);
                    sendResponse(response);
                 })
                 return true;
@@ -19,7 +24,7 @@ chrome.runtime.onMessage.addListener(
             	console.log('signup logic with formData = to', message.data);
               let userCreds = message.data;
               userCreds.username = message.data.email.split('@')[0];
-              ajaxCall("POST", "user/signup", userCreds, function(response){
+              ajaxCall("POST", "user/signup", userCreds, '', function(response){
                   console.log('response from server is: ',response);
                   sendResponse(response);
               })
@@ -31,11 +36,14 @@ chrome.runtime.onMessage.addListener(
 });
 
 
-function ajaxCall(type, path, data, callback){
+function ajaxCall(type, path, data, token, callback){
   $.ajax({
     url: domain+path,
     type: type,
     data: data,
+    headers: {
+        token: token
+    },
     success: function(response){
       console.log('response: ', response)
       callback(response);
@@ -45,4 +53,16 @@ function ajaxCall(type, path, data, callback){
       callback(response);
     }
   });
+}
+
+function setStorageItem(varName, data){
+  console.log('varName: ', varName);
+  if(varName!='searchPageData'){
+    console.log('data', data);
+    window.localStorage.setItem(varName, JSON.stringify(data));
+  }
+}
+
+function getStorageItem(varName){
+  return JSON.parse(localStorage.getItem(varName));
 }
