9149161069c65a32ee7e6320f55b0f0000d118c9
Elisha Kramer <kramer1346@gmail.com>
2020-06-16 13:24:37 +0300

    Successfully Looping from Page to Page of Search Results

--- Chrome_Extension/js/app/content.js
+++ Chrome_Extension/js/app/content.js
@@ -63,7 +63,15 @@ function sendToBackground(eventName, eventData, callback){
                     } else if (response.nextWhat == 'nextPage' && typeof response.year != 'undefined'){
                       window.location.href = 'https://www.amazon.com/gp/your-account/order-history/ref=ppx_yo_dt_b_pagination_1_2_3_4_5?ie=UTF8&orderFilter=year-'+response.year+'&search=&startIndex='+response.startIndex+'&ahf=on';
                     }
-                } 
+                } else if(eventName=='searchPageData'){
+                    console.log('searchPageData response block ran');
+                    if(response.nextWhat == 'nextPage'){
+                        window.location.href = 'https://www.amazon.com/s?k='+response.searchKeyword+'&asf=on&page='+response.nextPageNumber+'&i=stripbooks-intl-ship';
+                    } else if(response.nextWhat == 'nextKeyword'){
+                        console.log('reached nextKeyword conditional block');
+                        window.location.href = 'https://www.amazon.com/s?k='+response.searchKeyword+'&asf=on&page=1&i=stripbooks-intl-ship';
+                    }
+                }
             }
     );
 }
