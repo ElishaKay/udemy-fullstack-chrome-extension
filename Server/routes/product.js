5c36b1e9aeec777df254a464c07b323161d3a6d0
Elisha Kramer <kramer1346@gmail.com>
2020-06-16 08:48:41 +0300

    backend API structure for saving products from Search Results

--- Server/routes/product.js
+++ Server/routes/product.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // controllers
 const { saveProducts, savePage, saveProductsFromSearch, savePageFromSearch } = require('../controllers/product');
 
-router.post('/', saveProducts, savePage);
-// router.post('/extension/products-from-search', saveProductsFromSearch, savePageFromSearch);
+router.post('/products-from-history', saveProducts, savePage);
+router.post('/products-from-search', saveProductsFromSearch, savePageFromSearch);
 
 module.exports = router; 
